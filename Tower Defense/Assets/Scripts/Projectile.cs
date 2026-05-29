6d5b4bec61f028e5c5fa7077a6ce1b7e4b1863bd
Devon Almany <devonalmany@gmail.com>
2026-05-28 22:11:10 -0700

    feat: Complete lesson 11

---
+++ Tower Defense/Assets/Scripts/Projectile.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [field: SerializeField]
+    public float Speed { get; private set; } = 2;
+
+    [field: SerializeField]
+    public float Damage { get; private set; } = 1;
+
+    [field: SerializeField]
+    public Transform Target { get; private set; }
+
+    void Update()
+    {
+        if (Target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        transform.right = Target.position - transform.position;
+
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            Target.position,
+            Speed * Time.deltaTime
+        );
+
+        float distance = Vector3.Distance(transform.position, Target.position);
+
+        if (distance <= Mathf.Epsilon)
+        {
+            Health healthComponent = Target.GetComponentInParent<Health>();
+
+            if (healthComponent != null)
+            {
+                healthComponent.ApplyHit(this);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
