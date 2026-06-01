fc3bd1e921ec60d9cd1648f1c5673dfc30704f20
Devon Almany <devonalmany@gmail.com>
2026-06-01 16:25:15 -0700

    feat: Complete lesson 17

--- Tower Defense/Assets/Scripts/TurrentSpawner.cs
+++ Tower Defense/Assets/Scripts/TurrentSpawner.cs
@@ -8,20 +8,31 @@ public class TurrentSpawner : MonoBehaviour
     [field: SerializeField]
     public GameObject TargetGrid { get; private set; }
 
+    [field: SerializeField]
+    public PlayerController Controller { get; private set; }
+
     void OnEnable()
     {
+        Controller.InfoLabel.text = "Select a Tile";
         ListenToTilesIn(TargetGrid);
     }
 
     void OnDisable()
     {
         StopListeningToTilesIn(TargetGrid);
+
+        if (Controller != null && Controller.InfoLabel != null)
+        {
+            Controller.InfoLabel.text = "Click Build to Place a Turret";
+        }
     }
 
     public void ListenToTilesIn(GameObject grid)
     {
         foreach (TileController tile in grid.GetComponentsInChildren<TileController>())
         {
+            tile.OnCursorEnter.AddListener(ShowInfo);
+            tile.OnCursorExit.AddListener(ShowSelectTile);
             tile.OnCursorClicked.AddListener(SpawnTurrent);
         }
     }
@@ -35,13 +46,30 @@ public class TurrentSpawner : MonoBehaviour
 
         foreach (TileController tile in grid.GetComponentsInChildren<TileController>())
         {
+            tile.OnCursorEnter.RemoveListener(ShowInfo);
+            tile.OnCursorExit.RemoveListener(ShowSelectTile);
             tile.OnCursorClicked.RemoveListener(SpawnTurrent);
         }
     }
 
+    public bool CanSpawn(TileController tileController)
+    {
+        if (tileController.IsOccupied)
+        {
+            return false;
+        }
+
+        if (Controller.Gold < 50)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
     public void SpawnTurrent(TileController TileController)
     {
-        if (TileController.IsOccupied)
+        if (!CanSpawn(TileController))
         {
             return;
         }
@@ -50,5 +78,27 @@ public class TurrentSpawner : MonoBehaviour
         newTurrent.transform.position = TileController.transform.position;
 
         TileController.SetIsOccupied(true);
+        Controller.Gold -= 50;
+    }
+
+    public void ShowInfo(TileController tileController)
+    {
+        if (tileController.IsOccupied)
+        {
+            Controller.InfoLabel.text = "Cannot build here";
+        }
+        else if (Controller.Gold < 50)
+        {
+            Controller.InfoLabel.text = "<color=red>Not Enough Gold</color>";
+        }
+        else
+        {
+            Controller.InfoLabel.text = "50 Gold - Place Turret";
+        }
+    }
+
+    public void ShowSelectTile(TileController tileController)
+    {
+        Controller.InfoLabel.text = "Select a Tile";
     }
 }
