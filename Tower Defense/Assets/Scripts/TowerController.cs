6df85925736ee0c71d26455a2cd1961cc2c12694
Devon Almany <devonalmany@gmail.com>
2026-06-01 18:54:40 -0700

    feat: Complete lesson 20!!!

---
+++ Tower Defense/Assets/Scripts/TowerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TowerController : MonoBehaviour
+{
+    [field: SerializeField]
+    public float BaseHealth { get; private set; } = 5;
+
+    [field: SerializeField]
+    public float Damage { get; private set; } = 0;
+
+    [field: SerializeField]
+    public UnityEvent<TowerController> OnDestroyed { get; private set; }
+
+    public void ApplyHit(EnemyAttack attack)
+    {
+        Damage += attack.Damage;
+
+        Debug.Log("Tower hit. Damage is now: " + Damage);
+
+        Destroy(attack.gameObject);
+
+        if (Damage >= BaseHealth)
+        {
+         OnDestroyed.Invoke(this);
+         Destroy(gameObject);
+        }
+    }
+}
